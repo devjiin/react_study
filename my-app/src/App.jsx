bfbb8c0abe6599254e95ab82fe3c48a8de9c6f83
devjiin <2ddung@naver.com>
2025-07-03 17:30:22 +0900

    Feat: Tailwind CSS 적용

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./shared/Router";
 import Nav from "./components/Nav";
-import { Reset } from 'styled-reset'
 
 function App() {
   return (
     <BrowserRouter>
-      <Reset />
       <Nav />
       <Router />
     </BrowserRouter>
--- my-app/src/components/BrandShop/BrandShop.jsx
+++ my-app/src/components/BrandShop/BrandShop.jsx
@@ -4,16 +4,21 @@ import mockData from "../../../public/data/mockData.json";
 import "swiper/css";
 import "swiper/css/navigation";
 import 'swiper/css/pagination';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
+import { twMerge } from 'tailwind-merge'
 
-const getSwiperConfig = {
+const cards = mockData.brandshop.cards;
+
+  const getSwiperConfig = {
   loop: true,
   autoplay:{
     delay: 2500,
   },
+  loopAdditionalSlides: 2, // 여유 슬라이드 복제
   slidesPerView: "auto",
   centeredSlides: true,
+  // watchSlidesProgress: true,
   spaceBetween: 16,
   wrapperTag: "ul",
   navigation: {
@@ -22,36 +27,37 @@ const getSwiperConfig = {
   },
   pagination:{
     type: 'progressbar',
-  } 
-};
-
-const cards = mockData.brandshop.cards;
+  }
+}
 
 const BrandShop = () => {
-  const [isPressed, setIsPressed] = useState(false);
+  const [isPressed, setIsPressed] = useState(true);
   const [swiper, setSwiper] = useState(null);
   const handleClick = () => {
     setIsPressed((prev)=> !prev);
-    isPressed ? swiper.autoplay.start() : swiper.autoplay.stop();
+    isPressed ? swiper.autoplay.stop() : swiper.autoplay.start();
+    console.log(swiper.slides.length)
   }
 	return (
-    <div className="box__banner">
-      <button className={clsx('button__play-control',isPressed && 'button__play--pause')} aria-expanded={isPressed} onClick={handleClick}>
+    <div className="box__banner relative">
+      <button className={clsx(twMerge('button__play-control absolute bottom-[44px] right-[calc(50%-190px)] z-10 w-[40px] h-[40px]', isPressed || 'bg-amber-500'))} aria-pressed={isPressed} onClick={handleClick}>
         <span className="for-a11y">자동재생</span>
       </button>
-      <Swiper {...getSwiperConfig} modules={[Autoplay, Navigation, Pagination]} onSwiper={setSwiper}>
+      <Swiper {...getSwiperConfig} className="group overflow-visible relative max-w-[1200px] pb-[30px]!" onBeforeInit={(swiper) => {
+    swiper.params.loopedSlides = cards.length;
+  }} modules={[Autoplay, Navigation, Pagination]} onSwiper={setSwiper}>
       {cards.map((card)=>{
         const {cardId, title, text1, text2, imageUrl, landingUrl} = card;
         return (
           <SwiperSlide tag="li" key={cardId}>
-            <a className="link" href={landingUrl}>
-              <img src={imageUrl} alt="" className="image" />
-              <div className="box__text-wrap">
-                <strong className="text__title">{title}</strong>
+            <a className="link block overflow-hidden relative rounded-[8px] w-full h-full" href={landingUrl}>
+              <img src={imageUrl} alt="" className="image w-full h-full object-cover" />
+              <div className="box__text-wrap opacity-0 absolute b-[38px] px-[40px]">
+                <strong className="text__title text-[28px] font-bold">{title}</strong>
                 {text1 && 
-                  <p className="text">
+                  <p className="text text-[15px] text-[#424242]">
                     {text1}
-                    {text2 && <><br />{text2}</>}
+                    {text2 && <><br />{text2}</>} 
                   </p>
                 }
               </div>
@@ -59,8 +65,8 @@ const BrandShop = () => {
           </SwiperSlide>
         )
       })}
-      <button className="swiper-button-prev" aria-label="이전 배너로 가기"></button>
-      <button className="swiper-button-next" aria-label="다음 배너로 가기"></button>
+      <button className="swiper-button-prev text-white opacity-0 group-hover:opacity-100" aria-label="이전 배너로 가기"></button>
+      <button className="swiper-button-next text-white opacity-0 group-hover:opacity-100" aria-label="다음 배너로 가기"></button>
     </Swiper>
     </div>
     
