1b0a0b6e3cdcaf329743faa017dd4b06e4195002
devjiin <2ddung@naver.com>
2025-07-09 00:48:47 +0900

    Feat: layer focus trap

--- my-app/src/components/Hotel/Hotel.jsx
+++ my-app/src/components/Hotel/Hotel.jsx
@@ -1,23 +1,42 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import Layer from "./Layer";
 import mockData from "../../../public/data/mockData.json";
 
-const buttonArr = ['버튼1', '버튼2', '버튼3'];
+const buttonArr = ["버튼1", "버튼2", "버튼3"];
 
-const Hotel = () =>{
+const Hotel = () => {
   const buttonRef = useRef([]);
   const [isOpen, setIsOpen] = useState(false);
-  const handleClickOpenLayer = () =>{
+  const [activeIdx, setActiveIdx] = useState(0);
+  const handleClickOpenLayer = (idx) => {
     setIsOpen(true);
-  }
-	return (
+    setActiveIdx(idx);
+  };
+  const foucsedButton = buttonRef.current[activeIdx];
+  return (
     <>
-      {buttonArr.map((btn, idx)=>{
-        return <button ref={el => buttonRef.current[idx] = el} key={idx} className="button" onClick={handleClickOpenLayer}>{btn}</button>
+      {buttonArr.map((btn, idx) => {
+        return (
+          <button
+            ref={(el) => (buttonRef.current[idx] = el)}
+            key={idx}
+            className="button"
+            onClick={() => handleClickOpenLayer(idx)}
+          >
+            {btn}
+          </button>
+        );
       })}
-      {isOpen && <Layer data={mockData.hotel} buttonRef={buttonRef} isOpen={isOpen} setIsOpen={setIsOpen} />}
+      {isOpen && foucsedButton && (
+        <Layer
+          data={mockData.hotel}
+          buttonRef={foucsedButton}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
+      )}
     </>
   );
-}
+};
 
 export default Hotel;
